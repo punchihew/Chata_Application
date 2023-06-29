f5e6d02bffc28a9a7e4a2bddbb6dd70a529d90a4
punchihewa <navishkapunchihewa66@gmail.com>
2023-06-29 12:50:14 +0530

    Create sErverDesing  Added

--- src/main/java/Server/server.java
+++ src/main/java/Server/server.java
@@ -1,6 +1,30 @@
 package Server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class server {
 
 
+    private static ArrayList<Handler> clients = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(4500);
+
+        Socket accept;
+
+
+        while (true) {
+
+            System.out.println("Waiting for Client ...");
+            accept = serverSocket.accept();
+            System.out.println("Client Connected");
+            Handler clientThread = new Handler(accept, clients);
+            clients.add(clientThread);
+            clientThread.start();
+        }
+    }
 }
