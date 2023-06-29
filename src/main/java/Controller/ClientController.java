cd9d5df3638469b8f9a28accc704fe6bd601735d
punchihewa <navishkapunchihewa66@gmail.com>
2023-06-29 12:58:54 +0530

    Create money and love

--- src/main/java/Controller/ClientController.java
+++ src/main/java/Controller/ClientController.java
@@ -98,9 +98,17 @@ public class ClientController {
     }
 
     public void money(MouseEvent mouseEvent) {
+
+        String emoji = new String(Character.toChars(129297));
+        textChat.setText(emoji);
+        imogi_pane.setVisible(false);
     }
 
     public void love(MouseEvent mouseEvent) {
+
+        String emoji = new String(Character.toChars(128525));
+        textChat.setText(emoji);
+        imogi_pane.setVisible(false);
     }
 
     public void green_sad(MouseEvent mouseEvent) {
