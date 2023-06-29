82326aa80b21f838d302fed9c76f295e64fddf43
punchihewa <navishkapunchihewa66@gmail.com>
2023-06-29 12:42:11 +0530

    Create LoginController  Added

---
+++ src/main/java/Controller/LoginController.java
@@ -0,0 +1,8 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+
+public class LoginController {
+    public void NameOnAction(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/Controller/LoginFromController.java
+++
@@ -1,7 +0,0 @@
-package Controller;
-
-public class LoginFromController {
-
-
-
-}
