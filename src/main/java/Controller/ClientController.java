dde6ada0a275482ef1af4ebda1e60c06276e07ea
punchihewa <navishkapunchihewa66@gmail.com>
2023-06-29 12:32:27 +0530

    Create clientController  Added

---
+++ src/main/java/Controller/ClientController.java
@@ -0,0 +1,72 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class ClientController {
+    public void btn_logout_On_action(MouseEvent mouseEvent) {
+    }
+
+    public void Mouse_On_Click_Hide_Emogi(MouseEvent mouseEvent) {
+    }
+
+    public void ChatOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btn_imogi_on_action(MouseEvent mouseEvent) {
+    }
+
+    public void btn_cam_on_action(MouseEvent mouseEvent) {
+    }
+
+    public void btn_massage_send_on_action(MouseEvent mouseEvent) {
+    }
+
+    public void emo_pane_on_action(MouseEvent mouseEvent) {
+    }
+
+    public void sad(MouseEvent mouseEvent) {
+    }
+
+    public void lot_sad(MouseEvent mouseEvent) {
+    }
+
+    public void money(MouseEvent mouseEvent) {
+    }
+
+    public void love(MouseEvent mouseEvent) {
+    }
+
+    public void green_sad(MouseEvent mouseEvent) {
+    }
+
+    public void smile_one_eyy(MouseEvent mouseEvent) {
+    }
+
+    public void cry_yes(MouseEvent mouseEvent) {
+    }
+
+    public void cry_head(MouseEvent mouseEvent) {
+    }
+
+    public void real_amile(MouseEvent mouseEvent) {
+    }
+
+    public void tuin(MouseEvent mouseEvent) {
+    }
+
+    public void woow(MouseEvent mouseEvent) {
+    }
+
+    public void smile_normal(MouseEvent mouseEvent) {
+    }
+
+    public void large_smile(MouseEvent mouseEvent) {
+    }
+
+    public void small_smile(MouseEvent mouseEvent) {
+    }
+
+    public void tong_smile(MouseEvent mouseEvent) {
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/view/client.fxml"));
+        Scene mainScene = new Scene(root);
+        stage.setScene(mainScene);
+        stage.setTitle("Java FX POS");
+        stage.centerOnScreen();
+        stage.show();
+
+
+    }
+}
